2c2fb198c155dec9e71af6ed82e9eea57f3d6414
stratisg <g.stratis@northeastern.edu>
2026-02-26 15:07:15 -0500

    Created simple dynamical model for parameter estimation.

---
+++ utilities.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import pyro
+import torch
+from pyro.distributions import Normal
+from pyro.infer import SVI, Trace_ELBO
+from pyro.optim import Adam
+
+
+def generate_data(mean, std, n_measurements):
+    """
+    True generating process.
+    """
+
+    return Normal(mean, std).sample((n_measurements,))
+
+
+def model_normal(mu_mean, std_mean, std_likelihood, measurements):
+    """
+    Prior distribution of the mean.
+    """
+    mean = pyro.sample("mean", Normal(mu_mean, std_mean))
+    n_samples = len(measurements)
+    with pyro.plate("measurements", n_samples):
+        pyro.sample("y", Normal(mean, std_likelihood),
+                    obs=measurements)
+    
+    return mean
+
+
+def inference(model, guide, model_args, optim_args, n_steps=1000, print_epoch=100):
+    """
+    Variational inference loop.
+    """
+    pyro.clear_param_store()
+    optimizer = Adam(optim_args)
+    svi = SVI(model, guide, optimizer, loss=Trace_ELBO())
+    l_loss = torch.zeros(n_steps)
+    for i_step in range(n_steps):
+        l_loss[i_step] = svi.step(**model_args)
+        if i_step % print_epoch == 0:
+            print(79 * "=")
+            print(f"Epoch: {i_step}\tLoss: {l_loss[i_step]:.3E}")
+    # Print last loss.
+    print(79 * "=")
+    print(f"Epoch: {i_step}\tLoss: {l_loss[i_step]:.3E}")
+
+    # Visualize the loss.
+    l_epochs = torch.arange(n_steps)
+    plt.figure("Loss")
+    if l_loss.any() <= 0:
+        plt.plot(l_epochs, l_loss)
+    else:
+        plt.semilogy(l_epochs, l_loss)
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
